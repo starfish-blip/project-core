6a4f002dec64fb4d1fb517452b60904eea6ecbd2
aroha-monorepo <terrystohr@gmail.com>
2026-09-19 14:11:39 +1200

    feat: add configuration module and corresponding test suite

---
+++ src/config.py
@@ -0,0 +1,6 @@
+﻿import os
+
+class Config:
+    ENV = os.getenv("ENV", "development")
+    DEBUG = ENV == "development"
+    VERSION = "1.0.0"
---
+++ tests/test_config.py
@@ -0,0 +1,5 @@
+﻿from config import Config
+
+def test_config():
+    assert Config.VERSION == "1.0.0"
+    assert isinstance(Config.DEBUG, bool)
